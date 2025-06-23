402c18804000fe5413acb0c7f172ab1fd25b5138
RealBugBear <bugbeardyl@gmail.com>
2025-06-23 20:42:56 +0200

    Add questionnaire results screen and profile saving

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ import 'package:bugbear_app/features/calendar/models/calendar_event_adapter.dart
 import 'package:bugbear_app/features/calendar/services/calendar_service.dart';
 import 'package:bugbear_app/features/questionnaire/questionnaire_screen.dart';
 import 'package:bugbear_app/features/questionnaire/quiz_intro_screen.dart';
-import 'package:bugbear_app/features/questionnaire/reflexe_profil_temp.dart';
+import 'package:bugbear_app/features/questionnaire/questionnaire_result_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -139,7 +139,7 @@ class MyApp extends StatelessWidget {
           '/calendar': (c) => const CalendarScreen(),
           '/questionnaire': (c) => const QuizIntroScreen(),
           '/questionnaire/questions': (c) => const QuestionnaireScreen(),
-          '/reflexe-profil-temp': (c) => const ReflexeProfilTemp(),
+          '/reflexe-profil-temp': (c) => const QuestionnaireResultScreen(),
         },
       ),
     );
