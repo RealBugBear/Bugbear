3241994ab281446d3b1ba5d77272cb86f367c820
RealBugBear <alexander.messinger@gmx.de>
2025-06-25 01:14:54 +0200

    removed unused test import

--- test/role_selection_screen_test.dart
+++ test/role_selection_screen_test.dart
@@ -1,5 +1,5 @@
 import 'package:bugbear_app/features/onboarding/screens/role_selection_screen.dart';
-import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
