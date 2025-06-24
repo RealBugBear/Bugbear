48b259bf76881aac58f537ab3c8eb04a13468458
RealBugBear <bugbeardyl@gmail.com>
2025-06-24 02:47:13 +0200

    Remove unused firebase_core import

--- lib/features/questionnaire/questionnaire_state.dart
+++ lib/features/questionnaire/questionnaire_state.dart
@@ -5,7 +5,6 @@ import 'package:flutter/services.dart' show rootBundle;
 import 'package:hive/hive.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:bugbear_app/features/profile/models/reflex_profile.dart';
 import 'package:bugbear_app/features/profile/services/profile_service.dart';
 
