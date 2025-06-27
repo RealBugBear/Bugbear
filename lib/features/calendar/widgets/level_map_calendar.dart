1154d3f23919f973fa3ae55b9876b9d58a12d101
RealBugBear <bugbeardyl@gmail.com>
2025-06-28 00:49:50 +0200

    Fix missing closing parenthesis in calendar widget

--- lib/features/calendar/widgets/level_map_calendar.dart
+++ lib/features/calendar/widgets/level_map_calendar.dart
@@ -154,6 +154,7 @@ class LevelMapCalendar extends StatelessWidget {
                               ),
                               ],
                           ),
+                        ),
                       );
                     },
                   ),
