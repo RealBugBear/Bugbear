45cc9a143e9805ad8ed19452c1019c97c53fb449
RealBugBear <bugbeardyl@gmail.com>
2025-06-28 08:07:32 +0200

    Fix month label overlay layout

--- lib/features/calendar/screens/calendar_screen.dart
+++ lib/features/calendar/screens/calendar_screen.dart
@@ -145,7 +145,6 @@ class _CalendarScreenContentState extends State<_CalendarScreenContent> {
                     ),
                     Positioned(
                       right: 0,
-
                       child: Padding(
                         padding: const EdgeInsets.symmetric(vertical: 8),
                         child: Container(
@@ -161,29 +160,6 @@ class _CalendarScreenContentState extends State<_CalendarScreenContent> {
                                 fontSize: 20,
                                 fontWeight: FontWeight.bold,
                               ),
-
-
-                      top: 8,
-                      bottom: 8,
-
-                      top: 0,
-                      bottom: 0,
-
-                      child: Container(
-                        width: 48,
-                        color: Colors.blueGrey.shade50,
-                        alignment: Alignment.center,
-                        child: RotatedBox(
-                          quarterTurns: 3,
-                          child: Text(
-                            monthName,
-
-                            textAlign: TextAlign.center,
-
-                            style: GoogleFonts.comicNeue(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-
                             ),
                           ),
                         ),
