8d8d03dd52e40f95af0ac258770fd8f6ac9cb400
RealBugBear <bugbeardyl@gmail.com>
2025-10-31 19:15:01 +0100

    Fix audio context setup imports

--- lib/features/progress/widgets/core_progress_section.dart
+++ lib/features/progress/widgets/core_progress_section.dart
@@ -1,10 +1,8 @@
 import 'dart:async';
 
 import 'package:audioplayers/audioplayers.dart';
-
-
+import 'package:audioplayers_platform_interface/audioplayers_platform_interface.dart';
 import 'package:flutter/foundation.dart';
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -94,11 +92,9 @@ class _CoreProgressSectionState extends State<CoreProgressSection> {
     super.initState();
     _windowKey = _windowKeyFor(widget.progress);
     _audioPlayer = AudioPlayer(playerId: 'progress_feedback_${hashCode}');
- codex/add-telemetry-service-hooks-to-core_progress_section
     unawaited(_audioPlayer.setReleaseMode(ReleaseMode.stop));
 
     unawaited(_configureAudioPlayer());
- gpt
   }
 
   @override
@@ -173,13 +169,11 @@ class _CoreProgressSectionState extends State<CoreProgressSection> {
     );
   }
 
- codex/add-telemetry-service-hooks-to-core_progress_section
-
   Future<void> _configureAudioPlayer() async {
     await _audioPlayer.setReleaseMode(ReleaseMode.stop);
     if (!kIsWeb) {
       await _audioPlayer.setAudioContext(
-        const AudioContext(
+        AudioContext(
           android: AudioContextAndroid(
             isSpeakerphoneOn: false,
             stayAwake: false,
@@ -195,8 +189,6 @@ class _CoreProgressSectionState extends State<CoreProgressSection> {
       );
     }
   }
-
- gpt
   ProgressFeedbackHooks _mergedFeedbackHooks() {
     final external = widget.feedbackHooks;
 
