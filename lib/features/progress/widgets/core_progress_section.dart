872ad1362661552063e4f8e9098f1f01a9465296
RealBugBear <bugbeardyl@gmail.com>
2025-10-31 19:07:34 +0100

    Remove stray merge markers from core progress section

--- lib/features/progress/widgets/core_progress_section.dart
+++ lib/features/progress/widgets/core_progress_section.dart
@@ -94,11 +94,9 @@ class _CoreProgressSectionState extends State<CoreProgressSection> {
     super.initState();
     _windowKey = _windowKeyFor(widget.progress);
     _audioPlayer = AudioPlayer(playerId: 'progress_feedback_${hashCode}');
- codex/add-telemetry-service-hooks-to-core_progress_section
     unawaited(_audioPlayer.setReleaseMode(ReleaseMode.stop));
 
     unawaited(_configureAudioPlayer());
- gpt
   }
 
   @override
@@ -173,8 +171,6 @@ class _CoreProgressSectionState extends State<CoreProgressSection> {
     );
   }
 
- codex/add-telemetry-service-hooks-to-core_progress_section
-
   Future<void> _configureAudioPlayer() async {
     await _audioPlayer.setReleaseMode(ReleaseMode.stop);
     if (!kIsWeb) {
@@ -195,8 +191,6 @@ class _CoreProgressSectionState extends State<CoreProgressSection> {
       );
     }
   }
-
- gpt
   ProgressFeedbackHooks _mergedFeedbackHooks() {
     final external = widget.feedbackHooks;
 
